c3274d47122ad0d5cd4049eb2aebd9ba17e52baa
Jerry Fan <huanyueyaoqin@qq.com>
2024-03-18 20:57:05 +0800

    feat: 增加PM2配置文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'Chat2DB 文档', // 应用名称
     script: 'npm',    // 运行脚本
-    args: 'build',    // 传递给脚本的参数
+    args: 'run build',    // 传递给脚本的参数
     env: {
       NODE_ENV: 'production', // 环境变量
       PORT: 3099,             // 你希望 Next.js 运行在哪个端口上
