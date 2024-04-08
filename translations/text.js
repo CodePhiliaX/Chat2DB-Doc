1368b7607d72144a472537a2eb28c29c68c14dd9
Jerry Fan <huanyueyaoqin@qq.com>
2024-04-08 21:02:17 +0800

    feat: 去掉繁体和土耳其语

--- translations/text.js
+++ translations/text.js
@@ -2,16 +2,16 @@
 export const languageMap = {
   "en-US": "English",
   "zh-CN": "简体中文",
-  "zh-TW": "繁體中文",
   "ja-JP": "日本語",
-  "tr-TR": "Türkçe",
+  // "zh-TW": "繁體中文",
+  // "tr-TR": "Türkçe",
 };
 
 /** @type {Readonly<Record<Locale, string>>} */
 export const tableOfContentsTitleMap = {
   "en-US": "On This Page",
   "zh-CN": "本页内容",
-  "zh-TW": "本頁內容",
   "ja-JP": "このページの内容",
-  "tr-TR": "Bu Sayfada",
+  // "zh-TW": "本頁內容",
+  // "tr-TR": "Bu Sayfada",
 };
