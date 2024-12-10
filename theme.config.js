72cbdb18888072a176505a1fda704b954edee91e
Jerry Fan <huanyueyaoqin@qq.com>
2024-12-10 08:05:49 +0800

    chore: 修改文档样式

--- theme.config.js
+++ theme.config.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { DiscordIcon, GitHubIcon } from "nextra/icons";
 
 export default {
   logo: (
@@ -43,13 +44,139 @@ export default {
   },
   footer: {
     text: (
-      <span>
-        MIT {new Date().getFullYear()} ©{" "}
-        <a href="https://chat2db.ai" target="_blank">
-          Chat2DB
-        </a>
-        .
-      </span>
+      <div style={{ width: "100%" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "start",
+            marginBottom: "20px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              gap: "40px",
+              height: "100%",
+            }}
+          >
+            <div style={{ display: "flex", gap: "20px" }}>
+              <a
+                href="https://discord.gg/uNjb3n5JVN"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <DiscordIcon size={24} />
+              </a>
+              <a
+                href="https://github.com/codePhiliaX/chat2db"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHubIcon size={24} />
+              </a>
+            </div>
+            <div style={{ fontSize: "16px", color: "#afaaaa" }}>
+              ©2024 Chat2DB. All rights reserved.
+            </div>
+          </div>
+          <div style={{ display: "flex", gap: "30px" }}>
+            <div>
+              <h4
+                style={{ margin: "0 0 10px 0", fontWeight: 600 }}
+                className="nx-text-slate-100"
+              >
+                Products
+              </h4>
+              <div
+                style={{ display: "flex", flexDirection: "column", gap: "5px" }}
+                className="nx-text-slate-300"
+              >
+                <a
+                  href="https://chat2db.ai/pricing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Pricing
+                </a>
+                <a
+                  href="https://github.com/codePhiliaX/Chat2DB"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="nx-text-slate-300"
+                >
+                  Community
+                </a>
+              </div>
+            </div>
+            <div>
+              <h4
+                style={{ margin: "0 0 10px 0", fontWeight: 600 }}
+                className="nx-text-slate-100"
+              >
+                Resources
+              </h4>
+              <div
+                style={{ display: "flex", flexDirection: "column", gap: "5px" }}
+                className="nx-text-slate-300"
+              >
+                <a
+                  href="https://chat2db.ai/download"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Download
+                </a>
+                <a
+                  href="https://chat2db.ai/quick-start"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Quick Start
+                </a>
+                <a
+                  href="https://chat2db.ai/resources/blog"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Blogs
+                </a>
+              </div>
+            </div>
+            <div>
+              <h4
+                style={{ margin: "0 0 10px 0", fontWeight: 600 }}
+                className="nx-text-slate-100"
+              >
+                Company
+              </h4>
+              <div
+                style={{ display: "flex", flexDirection: "column", gap: "5px" }}
+                className="nx-text-slate-300"
+              >
+                <a
+                  href="https://chat2db.ai/resources/docs/start-guide/about-chat2db"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="nx-text-slate-300"
+                >
+                  About
+                </a>
+                <a
+                  href="https://chat2db.ai/resources/docs/contact/contact"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="nx-text-slate-300"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     ),
   },
   // i18n: Object.entries(languageMap).map(([locale, text]) => ({
