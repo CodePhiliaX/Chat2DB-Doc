6e794f7cba6dced8e0229568fec01228afa7c86e
Jerry Fan <huanyueyaoqin@qq.com>
2024-12-17 17:55:51 +0800

    chore: 修改

--- script/deploy_script.sh
+++ script/deploy_script.sh
@@ -15,10 +15,10 @@ echo "构建成功"
 
 # 压缩 .next 目录和 public  文件夹
 echo "压缩 .next 目录和 public  文件夹..."
-tar -czf deploy.tar.gz .next public 
+# tar -czf deploy.tar.gz .next public 
 
 echo "传输文件到国外服务器..."
-scp deploy.tar.gz package.json package-lock.json root@47.89.251.23:/root/Chat2DB-Doc
+# scp deploy.tar.gz package.json package-lock.json root@47.89.251.23:/root/Chat2DB-Doc
 
 echo "在国外服务器上解压文件, 并重启服务"
 ssh root@47.89.251.23 "cd /root/Chat2DB-Doc && rm -rf .next && tar -xzvf deploy.tar.gz --warning=no-unknown-keyword && pm2 restart Chat2DB-Doc"
